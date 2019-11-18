f58b6602c0d7ab6f928c74ce0ec8066cacf99ce8
mohit6350quinnox <11codes.mohit@gmail.com>
2019-11-18 10:59:46 +0530

    added login controller

--- DigitalGarage-V1/src/main/java/com/quinnox/service/controller/LoginController.java
+++ DigitalGarage-V1/src/main/java/com/quinnox/service/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.quinnox.service.controller;
 
+import static org.hamcrest.CoreMatchers.containsString;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,37 +20,48 @@ public class LoginController {
 
 	@PostMapping(value="/login", consumes="application/json")
 	public String userLogin(@RequestBody DGUser user) {
-		String password = user.getUserPassword();
-		String userEmail = user.getUserEmail();
-		String userName = user.getUserName();
-		DGUser activeUser= null;
-		
-		if(activeUser == null) {
-			return "invalid email";
-		}
-		if(activeUser == null) {
-			return "invalid usreName";
-		}
-		
-		if(password != null && userEmail != null){
-			activeUser = userRepository.getUserByEmail(userEmail);
-			if(activeUser.getUserEmail().equals(userEmail) && activeUser.getUserPassword().equals(password)) {
-				return activeUser.getUserId();
-			}else {
-				return "invalid user";
-			}
-		}else if (password != null && userName != null) {
-			activeUser = userRepository.getUserByUserName(userName);
-			if(activeUser.getUserName().equals(userName) && activeUser.getUserPassword().equals(password))  {
-				return activeUser.getUserId();
-			}else {
-				return "invalid user";
-			}
-		}
-		else
-			return "UserName/Email or Password is invalid";
-		
-		
+	  String key = null;
+	  String response = null;
+	  DGUser dbUser = null;
+	  
+	  // check to userName/email is not null ==========================
+	  
+	  if(user.getUserEmail() != null){
+		  System.err.println("email is entered");
+		  key = user.getUserEmail();
+		    if(user.getUserPassword() != null) {
+		    	try {
+		    		dbUser =  userRepository.getUserByEmail(key);
+		    		if(user.getUserEmail().equals(dbUser.getUserEmail()) && user.getUserPassword().equals(dbUser.getUserPassword())){
+		    			response = dbUser.getUserId();
+		    		}else {
+		    			response = "Invalid userName or password";
+		    		}
+		    	}catch(Exception e) {
+		    		response =  "Invalid userName or password";
+		    	}
+		    }
+		    return response;
+	  }else if(user.getUserName() != null){
+		  System.err.println("userName is entered");
+		  key = user.getUserName();
+		    if(user.getUserPassword() != null) {
+		    	try {
+		    		dbUser =  userRepository.getUserByUserName(key);
+		    		if(user.getUserName().equals(dbUser.getUserName()) && user.getUserPassword().equals(dbUser.getUserPassword())){
+		    			response = dbUser.getUserId();
+		    		}else {
+		    			response = "Invalid userName or password";
+		    		}
+		    	}catch(Exception e) {
+		    		response =  "Invalid userName or password";
+		    	}
+		    }
+		    return response;
+	  }
+	  else
+		  return "UserName or Email can not be empty";
+	  	
 	}
 	
 }
